277b255d52330122f0597a95080996b1ba921181
masa <masatoshi.sboard@gmail.com>
2021-03-18 17:10:36 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,8 +1,25 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::collections::BinaryHeap;
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut m: usize,
+        mut a: [usize; n],
     }
+    let mut queue = BinaryHeap::new();
+    for i in 0..n {
+        queue.push(a[i]);
+    }
+    for _ in 0..m {
+        let t = queue.pop().unwrap();
+        queue.push(t / 2);
+    }
+    let mut ans = 0;
+    while !queue.is_empty() {
+        ans += queue.pop().unwrap();
+    }
+    println!("{}", ans);
 }
